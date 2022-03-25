4eee43c6b5a76026d1c6f83a611cc8a4eb360562
Davis Gordon Dun <davis.dun@gmail.com>
2022-03-24 23:29:58 -0300

    third apps removed

--- tedrosbox/app-somos/app-somos-fx/src/main/java/org/somos/start/TConstant.java
+++
@@ -1,7 +0,0 @@
-package org.somos.start;
-
-public interface TConstant {
-
-	public static final String UUI = "SOMOS_20220123";
-	
-}
--- tedrosbox/editor-web/editor-web-fx/src/main/java/com/editorweb/start/TConstant.java
+++
@@ -1,7 +0,0 @@
-package com.editorweb.start;
-
-public interface TConstant {
-
-	public static final String UUI = "EDITORWEB_20211005";
-	
-}
