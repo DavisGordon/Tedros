a42a61e9cd6ff8628bbb2e0f4cce4c44ca0e84d9
Davis Gordon Dun <davis.dun@gmail.com>
2022-10-20 21:40:39 -0300

    ChatSetting

---
+++ tedrosbox/app-chat/app-chat-fx/src/main/java/org/tedros/chat/module/client/setting/ChatUtil.java
@@ -0,0 +1,186 @@
+/**
+ * 
+ */
+package org.tedros.chat.module.client.setting;
+
+import java.io.ByteArrayInputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.text.DateFormat;
+import java.util.Date;
+
+import org.apache.commons.lang3.ArrayUtils;
+import org.tedros.chat.entity.ChatMessage;
+import org.tedros.common.model.TFileEntity;
+import org.tedros.core.TLanguage;
+import org.tedros.fx.TFxKey;
+import org.tedros.fx.control.TText;
+import org.tedros.fx.control.TText.TTextStyle;
+import org.tedros.fx.domain.TImageExtension;
+import org.tedros.fx.property.TBytesLoader;
+import org.tedros.server.entity.ITFileEntity;
+
+import javafx.beans.property.SimpleObjectProperty;
+import javafx.beans.value.ChangeListener;
+import javafx.beans.value.ObservableValue;
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
+import javafx.geometry.Insets;
+import javafx.geometry.Pos;
+import javafx.scene.Node;
+import javafx.scene.control.Hyperlink;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
+import javafx.scene.input.Clipboard;
+import javafx.scene.input.ClipboardContent;
+import javafx.scene.layout.HBox;
+import javafx.scene.layout.StackPane;
+import javafx.scene.layout.VBox;
+
+/**
+ * @author Davis Gordon
+ *
+ */
+public class ChatUtil {
+	
+	private TLanguage iEngine = TLanguage.getInstance();
+
+	/**
+	 * 
+	 */
+	public ChatUtil() {
+		// TODO Auto-generated constructor stub
+	}
+	
+	public StackPane buildTextPane(ChatMessage msg, boolean left) {
+		String user = msg.getFrom().getName();
+		String txt = msg.getContent();
+		final TFileEntity file = msg.getFile();
+		Date dt =  msg.getInsertDate();
+		String dtf = dt!=null 
+				? DateFormat
+				.getDateTimeInstance(DateFormat.MEDIUM, DateFormat.DEFAULT, TLanguage.getLocale())
+				.format(dt)
+				: "";
+		
+		VBox gp = new VBox(8);
+		HBox header = new HBox(4);
+		header.setId("t-chat-msg-header");
+		gp.getChildren().add(header);
+		if(user!=null) {
+			TText t2 = new TText(user);
+			t2.settTextStyle(TTextStyle.MEDIUM);
+			header.getChildren().add(t2);
+		}
+		HBox footer = new HBox(10);
+		footer.setAlignment(Pos.CENTER_LEFT);
+		footer.setId("t-chat-msg-footer");
+		if(!"".equals(dtf)) {
+			TText t2 = new TText(dtf);
+			t2.settTextStyle(TTextStyle.SMALL);
+			footer.getChildren().add(t2);
+		}
+		if(txt!=null) {
+			TText t1 = new TText(txt);
+			t1.settTextStyle(TTextStyle.MEDIUM);
+			t1.setWrappingWidth(300);
+			VBox.setMargin(t1, new Insets(8));
+			gp.getChildren().add(t1);
+			Hyperlink hl = new Hyperlink(iEngine.getString(TFxKey.BUTTON_COPY));
+			hl.getStyleClass().add(TTextStyle.SMALL.getValue());
+			hl.setUserData(txt);
+			EventHandler<ActionEvent> ev = e -> {
+				String text = (String) ((Node) e.getSource()).getUserData();
+				final Clipboard clipboard = Clipboard.getSystemClipboard();
+			    final ClipboardContent content = new ClipboardContent();
+			    content.putString(text);
+			    clipboard.setContent(content);
+			};
+			hl.setOnAction(ev);
+			footer.getChildren().add(hl);
+			
+		}else if(file!=null){
+			TText t1 = new TText(file.toString());
+			t1.settTextStyle(TTextStyle.MEDIUM);
+			t1.setWrappingWidth(300);
+			VBox.setMargin(t1, new Insets(8));
+			gp.getChildren().add(t1);
+			
+			Hyperlink hl = new Hyperlink(iEngine.getString(TFxKey.BUTTON_OPEN));
+			hl.getStyleClass().add(TTextStyle.SMALL.getValue());
+			EventHandler<ActionEvent> ev = e -> {
+				//TFileUtil..open(f)
+			};
+			hl.setOnAction(ev);
+			footer.getChildren().add(hl);
+			
+			String ext = file.getFileExtension();
+			String[] exts = TImageExtension.ALL_IMAGES.getExtensionName();
+			if(ArrayUtils.contains(exts, ext.toLowerCase())) {
+				ImageView iv = new ImageView();
+				iv.setFitWidth(300);
+				iv.setPreserveRatio(true);
+				if(!file.isNew()) {
+					this.loadImage(iv, file);
+				}else {
+					this.setImage(iv, file.getByteEntity().getBytes());
+				}
+
+				VBox.setMargin(iv, new Insets(8));
+				gp.getChildren().add(iv);
+			}
+		}
+		gp.getChildren().add(footer);
+		StackPane p1 = new StackPane();
+		p1.setId("t-chat-msg-pane");
+		p1.getChildren().add(gp);
+		p1.setAlignment(left ? Pos.CENTER_LEFT : Pos.CENTER_RIGHT);
+		return p1;
+	}
+	
+
+	/**
+	 * @param imgView
+	 * @param fe
+	 */
+	private void loadImage(final ImageView imgView, ITFileEntity fe) {
+		SimpleObjectProperty<byte[]> bp = new SimpleObjectProperty<>();
+		bp.addListener(new ChangeListener<byte[]>() {
+			@Override
+			public void changed(ObservableValue<? extends byte[]> arg0, byte[] arg1,
+					byte[] n) {
+				try {
+					fe.getByteEntity().setBytes(n);
+					setImage(imgView, n);
+				} catch (Exception e1) {
+					e1.printStackTrace();
+				}
+				bp.removeListener(this);
+			}
+		});
+		loadBytes(bp, fe);
+	}
+	
+	private void loadBytes(SimpleObjectProperty<byte[]> bp, ITFileEntity m) {
+		try {
+			TBytesLoader.loadBytesFromTFileEntity(m.getByteEntity().getId(), bp);
+		} catch (Throwable e) {
+			e.printStackTrace();
+		}
+	}
+	
+	/**
+	 * @param imgView
+	 * @param bytes
+	 * @throws IOException
+	 */
+	private void setImage(final ImageView imgView, byte[] bytes) {
+		try(InputStream is = new ByteArrayInputStream(bytes)){
+			Image img = new Image(is);
+			imgView.setImage(img);
+		} catch (IOException e) {
+			throw new RuntimeException(e);
+		}
+	}
+
+}
--- tedrosbox/tdrs-fx/tedros-fx-api/src/main/java/org/tedros/fx/annotation/parser/TBorderPaneParser.java
+++ tedrosbox/tdrs-fx/tedros-fx-api/src/main/java/org/tedros/fx/annotation/parser/TBorderPaneParser.java
@@ -2,10 +2,15 @@ package org.tedros.fx.annotation.parser;
 
 import java.lang.annotation.Annotation;
 
+import org.apache.commons.lang3.ArrayUtils;
 import org.tedros.api.descriptor.ITFieldDescriptor;
 import org.tedros.fx.annotation.layout.TBorderPane;
 
+import com.openhtmltopdf.util.ArrayUtil;
+
 import javafx.scene.Node;
+import javafx.scene.control.ScrollPane;
+import javafx.scene.control.ScrollPane.ScrollBarPolicy;
 import javafx.scene.layout.BorderPane;
 
 public class TBorderPaneParser extends TAnnotationParser<Annotation, BorderPane> {
@@ -17,40 +22,77 @@ public class TBorderPaneParser extends TAnnotationParser<Annotation, BorderPane>
 		
 		if(!"".equals(ann.top())) {
 			Node node = getNode(ann.top());
-			if(node!=null)
-				object.setTop(node);
-			else
+			if(node!=null) {
+				if(ann.scroll().length>0 && ArrayUtils.contains(ann.scroll(), ann.top()) ) {
+					ScrollPane sp = buildScrollPane(node);
+					object.setTop(sp);
+				}else
+					object.setTop(node);
+			}else
 				throwException(ann.top());
 		}
 		if(!"".equals(ann.right())) {
 			Node node = getNode(ann.right());
-			if(node!=null)
-				object.setRight(node);
-			else
+			if(node!=null) {
+				if(ann.scroll().length>0 && ArrayUtils.contains(ann.scroll(), ann.right()) ) {
+					ScrollPane sp = buildScrollPane(node);
+					object.setRight(sp);
+				}else
+					object.setRight(node);
+			}else
 				throwException(ann.right());
 		}
 		if(!"".equals(ann.bottom())) {
 			Node node = getNode(ann.bottom());
-			if(node!=null)
-				object.setBottom(node);
-			else
+			if(node!=null) {
+				if(ann.scroll().length>0 && ArrayUtils.contains(ann.scroll(), ann.bottom()) ) {
+					ScrollPane sp = buildScrollPane(node);
+					object.setBottom(sp);
+				}else
+					object.setBottom(node);
+			}else
 				throwException(ann.bottom());
 		}
 		if(!"".equals(ann.left())) {
 			Node node = getNode(ann.left());
-			if(node!=null)
-				object.setLeft(node);
-			else
+			if(node!=null) {
+				if(ann.scroll().length>0 && ArrayUtils.contains(ann.scroll(), ann.left()) ) {
+					ScrollPane sp = buildScrollPane(node);
+					object.setLeft(sp);
+				}else
+					object.setLeft(node);
+			}else
 				throwException(ann.left());
 		}
 		if(!"".equals(ann.center())) {
 			Node node = getNode(ann.center());
-			if(node!=null)
-				object.setCenter(node);
-			else
+			if(node!=null) {
+				if(ann.scroll().length>0 && ArrayUtils.contains(ann.scroll(), ann.center()) ) {
+					ScrollPane sp = buildScrollPane(node);
+					object.setCenter(sp);
+				}else
+					object.setCenter(node);
+			}else
 				throwException(ann.center());
 		}
-		super.parse(annotation, object, "top", "right", "bottom", "left", "center");
+		super.parse(annotation, object, "top", "right", "bottom", "left", "center", "scroll");
+	}
+
+
+	/**
+	 * @param node
+	 * @return
+	 */
+	private ScrollPane buildScrollPane(Node node) {
+		ScrollPane sp = new ScrollPane();
+		sp.setFitToHeight(true);
+		sp.setFitToWidth(true);
+		sp.autosize();
+		sp.setHbarPolicy(ScrollBarPolicy.AS_NEEDED);
+		sp.setVbarPolicy(ScrollBarPolicy.AS_NEEDED);
+		sp.setStyle("-fx-background-color: transparent;");
+		sp.setContent(node);
+		return sp;
 	}
 
 
