830b28115a862c15018cccabab83856b65bcf331
Davis Gordon Dun <davis.dun@gmail.com>
2023-01-17 23:08:24 -0300

    rest base api

---
+++ tedrosbox/tdrs-server/tedros-webapp/src/main/java/org/tedros/web/rest/ApplicationConfig.java
@@ -0,0 +1,16 @@
+/**
+ * 
+ */
+package org.tedros.web.rest;
+
+import javax.ws.rs.ApplicationPath;
+import javax.ws.rs.core.Application;
+
+/**
+ * @author Davis Gordon
+ *
+ */
+@ApplicationPath("/api")
+public class ApplicationConfig extends Application {
+   
+}
