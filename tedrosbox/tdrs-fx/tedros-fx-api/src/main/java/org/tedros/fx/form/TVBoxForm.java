8ac1e066d091b614a0c0b196dab9590aeb16c05d
Davis Gordon Dun <davis.dun@gmail.com>
2024-03-25 18:45:08 -0300

    update

--- tedrosbox/tdrs-fx/tedros-fx-api/src/main/java/org/tedros/fx/form/TVBoxForm.java
+++ tedrosbox/tdrs-fx/tedros-fx-api/src/main/java/org/tedros/fx/form/TVBoxForm.java
@@ -18,7 +18,6 @@ import org.tedros.api.presenter.view.TViewMode;
 import org.tedros.core.model.ITModelView;
 import org.tedros.core.repository.TRepository;
 
-import javafx.application.Platform;
 import javafx.beans.property.ReadOnlyBooleanProperty;
 import javafx.collections.ObservableList;
 import javafx.geometry.Insets;
