c711a2dbdb256c3f48ff25aa7d06fbf63694f65f
Tedros <tedrosbox@gmail.com>
2025-10-22 16:44:29 -0300

    melhorias

--- tedrosbox/tdrs-box/tedros-core/src/main/java/org/tedros/ai/function/model/ViewPath.java
+++ tedrosbox/tdrs-box/tedros-core/src/main/java/org/tedros/ai/function/model/ViewPath.java
@@ -3,6 +3,8 @@
  */
 package org.tedros.ai.function.model;
 
+import java.util.Objects;
+
 import com.fasterxml.jackson.annotation.JsonClassDescription;
 import com.fasterxml.jackson.annotation.JsonPropertyDescription;
 
@@ -11,7 +13,7 @@ import com.fasterxml.jackson.annotation.JsonPropertyDescription;
  *
  */
 @JsonClassDescription("the path to call a view")
-public class ViewPath {
+public class ViewPath implements Comparable<ViewPath> {
 	
 	@JsonPropertyDescription("The view path use it to open a view in the system. Don't change it!")
 	private String viewPath;
@@ -43,4 +45,37 @@ public class ViewPath {
 		this.viewPath = viewPath;
 	}
 
+	@Override
+	public int hashCode() {
+		return Objects.hash(viewPath);
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj) {
+			return true;
+		}
+		if (!(obj instanceof ViewPath)) {
+			return false;
+		}
+		ViewPath other = (ViewPath) obj;
+		return Objects.equals(viewPath, other.viewPath);
+	}
+
+	/* (non-Javadoc)
+	 * @see java.lang.Comparable#compareTo(java.lang.Object)
+	 */
+	@Override
+	public int compareTo(ViewPath other) {
+		if (this.viewPath == null && other.viewPath == null) {
+			return 0;
+		}
+		if (this.viewPath == null) {
+			return -1;
+		}
+		if (other.viewPath == null) {
+			return 1;
+		}
+		return this.viewPath.compareTo(other.viewPath);
+	}
 }
